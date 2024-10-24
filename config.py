90a0e71900775a7d22ae58045c7c295cdb1df9c7
Ronin <bhabyaprakash01@gmail.com>
2024-10-24 19:23:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = "your_api_id"
+API_HASH = "your_api_hash"
+START_VIDEO = "your_start_video_link"
+SUPPORT_CHAT = "your_support_chat_link"
+SUPPORT_CHANNEL = "your_support_channel_link"
+OWNER_USERNAME = "your_owner_username"
+BOT_USERNAME = "your_bot_username"
+MONGO_DB_URI = "your_mongo_db_uri"
+BOT_TOKEN = "your_bot_token"
+
+Helpers = [123456789]
